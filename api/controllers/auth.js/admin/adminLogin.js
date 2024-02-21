19932ce09dc768eb6524ded2f165fe2fca8dc5c1
devaharish2004 <devaharishm@gmail.com>
2024-02-21 14:34:19 +0530

    Authentication complete

--- api/controllers/auth.js/admin/adminLogin.js
+++ api/controllers/auth.js/admin/adminLogin.js
@@ -1,9 +1,52 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const pool = require("../../../db/db.js")
+
 const adminLogin = async (req, res) => {
     
     try {
+
+        const { username, password } = req.body;
+
+        const client = await pool.connect();
+
+        const search_query = {
+            text : "SELECT * FROM admin WHERE username = $1",
+            values : [username]
+        };
+
+        const result = await client.query(search_query);
+
+        if(result.rows.length === 0) {
+            res.status(404).json({message : "User not found. Please register"});
+        }
+        else {
+
+            const user = result.rows[0];
+
+            //check the passwords are matching or not
+
+            const passwordMatch = await bcrypt.compare(password, user.password);
+
+            if(passwordMatch) {
+                //generate the token
+                
+                const token = jwt.sign({ category : "admin", user : user.id }, "SECRET_KEY");
+
+                delete user.password;
+
+                res.status(200).json({message : {token, user, category : "admin"}});
+
+            }
+            else {
+                res.status(401).json({message : "Invalid credentials"});
+            }
+
+
+        }
         
     } catch (err) {
-
+        res.status(500).json({message : err.message});
     }
 
 };
--- api/controllers/auth.js/farmer/farmerLogin.js
+++ api/controllers/auth.js/farmer/farmerLogin.js
@@ -1,9 +1,47 @@
+const jwt = require("jsonwebtoken");
+const pool = require("../../../db/db.js")
+
 const farmerLogin = async (req, res) => {
+    
     try {
 
-    } catch (err) {
+        const { aadhaar_number, phone_number } = req.body;
 
+        const client = await pool.connect();
+
+        const search_query = {
+            text : "SELECT * FROM farmers WHERE aadhaar_number = $1",
+            values : [aadhaar_number]
+        };
+
+        const result = await client.query(search_query);
+
+        if(result.rows.length === 0) {
+            res.status(404).json({message : "User not found. Please register"});
+        }
+        else {
+
+            const user = result.rows[0];
+
+            //check if the phone number is matching or not
+
+            if(phone_number === user.phone_number) {
+                //generate the token
+                
+                const token = jwt.sign({ category : "farmer", user : user.id }, "SECRET_KEY");
+
+                res.status(200).json({token, user, category : "farmer"});
+
+            }
+            else {
+                res.status(401).json({message : "Invalid credentials"});
+            }
+        }
+        
+    } catch (err) {
+        res.status(500).json({message : err.message});
     }
+    
 };
 
 module.exports = farmerLogin;
